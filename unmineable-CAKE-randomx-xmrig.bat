2cb1bf4ce49a4a0b1a45419af9117261b54fd567
Aank is ME <email@aank.me>
2023-04-15 06:42:09 +0200

    Add files via upload

---
+++ unmineable-CAKE-randomx-xmrig.bat
@@ -0,0 +1,2 @@
+xmrig.exe -a rx -o stratum+ssl://rx.unmineable.com:443 -u CAKE:0x20bc18f12e8ee5ffbfd0e5022355b7913cbe3109.aanx -p x 
+pause
---
+++ unmineable-SOL-randomx-xmrig.bat
@@ -0,0 +1,2 @@
+xmrig.exe -a rx -o stratum+ssl://rx.unmineable.com:443 -u SOL:73WVZaVUcBctWAChxpAC1xPCuMHJiUxBnfLvqZqDJ2ip.aanx_solo -p x 
+pause
